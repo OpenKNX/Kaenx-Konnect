4d962391a89df230c2ddc0129a03fd9d2b83f38f
Dennis <ostermann3k@googlemail.com>
2026-02-27 09:09:14 +0100

    Added Timeout for GroupValueRead

--- Classes/BusCommon.cs
+++ Classes/BusCommon.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using Kaenx.Konnect.Addresses;
+﻿using Kaenx.Konnect.Addresses;
 using Kaenx.Konnect.Connections;
 using Kaenx.Konnect.EMI.DataMessages;
 using Kaenx.Konnect.EMI.LData;
+using Kaenx.Konnect.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -25,16 +27,6 @@ namespace Kaenx.Konnect.Classes
             responses[response.SequenceNumber] = response;
         }
 
-        private async Task<LDataBase> WaitForData(byte seq)
-        {
-            responses.Remove(seq);
-            while (!responses.ContainsKey(seq))
-                await Task.Delay(10);
-            var resp = responses[seq];
-            responses.Remove(seq);
-            return resp;
-        }
-
         public async Task GroupValueWrite(MulticastAddress ga, byte data)
         {
             await GroupValueWrite(ga, new byte[] { data });
@@ -49,12 +41,37 @@ namespace Kaenx.Konnect.Classes
             await _conn.SendAsync(message);
         }
 
-        public async Task<LDataBase> GroupValueRead(MulticastAddress ga)
+        public async Task<LDataBase?> GroupValueRead(MulticastAddress ga)
         {
-            var content = new GroupValueRead();
-            LDataBase message = new LDataBase(ga, false, 0, content);
-            int seq = await _conn.SendAsync(message);
-            return await WaitForData((byte)seq);
+            TaskCompletionSource<LDataBase> tcs = new TaskCompletionSource<LDataBase>();
+
+            void ReceivedMessage(LDataBase response)
+            {
+                if (response.GetApciType() == ApciTypes.GroupValueResponse &&
+                    response.DestinationAddress is MulticastAddress destGa &&
+                    destGa.GetBytes().SequenceEqual(ga.GetBytes()))
+                {
+                    tcs.TrySetResult(response);
+                }
+            }
+
+            _conn.OnReceivedMessage += ReceivedMessage;
+
+            try
+            {
+                var content = new GroupValueRead();
+                LDataBase message = new LDataBase(ga, false, 0, content);
+                await _conn.SendAsync(message);
+
+                if (await Task.WhenAny(tcs.Task, Task.Delay(3000)) != tcs.Task)
+                    return null;
+
+                return await tcs.Task;
+            }
+            finally
+            {
+                _conn.OnReceivedMessage -= ReceivedMessage;
+            }
         }
 
         public async Task IndividualAddressRead()
